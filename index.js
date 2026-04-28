149c70755a434767903ec49d96bf24a71e579206
gtmmanni-codes <gtmmanni@gmail.com>
2026-04-28 18:54:14 +0200

    Adding JSON format data by stringify it

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let http = require("http");
 let server = http.createServer((req,res)=>{
     if(req.url=="/home"){
-        res.end("Home Data here")
+        let data =[{
+            name:"Manni",
+            age:23,
+            Gender:"Male"
+        },{
+            name:"Harsh",
+            age:25,
+            Gender:"Male"
+        }];
+
+        res.end("Home Data here"+JSON.stringify(data));
     }else if(req.url == "/news"){
         res.end("New Data here")
     }else if(req.url =="/about"){
